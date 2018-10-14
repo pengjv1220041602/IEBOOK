3fb00d2fbdea6fe26a9055729ac6612fa194b664
peng <1220041602@qq.com>
2018-10-14 20:34:12 +0800

    添加思维导图

---
+++ 导图/spring boot 技术.mm
@@ -0,0 +1,21 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1539518982282" ID="ID_1161794674" MODIFIED="1539518992276" TEXT="spring boot &#x6280;&#x672f;">
+<node CREATED="1539518994737" ID="ID_254550936" MODIFIED="1539518996864" POSITION="right" TEXT="spring">
+<node CREATED="1539519074481" ID="ID_215972602" MODIFIED="1539519077260" TEXT="ioc">
+<node CREATED="1539519086906" ID="ID_664207400" MODIFIED="1539519110714" TEXT="ioc &#x63a7;&#x5236;&#x53cd;&#x8f6c;&#xff0c;&#x5c06;&#x521b;&#x5efa;&#x5bf9;&#x8c61;&#x6258;&#x4ed8;&#x7ed9;spring&#x5bb9;&#x5668;&#x8fdb;&#x884c;&#x7ba1;&#x7406;"/>
+</node>
+<node CREATED="1539519080544" ID="ID_558772119" MODIFIED="1539519082181" TEXT="aop">
+<node CREATED="1539519117969" ID="ID_1507547823" MODIFIED="1539519167994" TEXT="aop &#x9762;&#x5411;&#x5207;&#x9762;&#xff0c;&#x5728;&#x7a0b;&#x5e8f;&#x4e2d;&#xff0c;&#x4e0d;&#x5f71;&#x54cd;&#x6574;&#x4e2a;&#x6d41;&#x7a0b;&#xff0c;&#x8fdb;&#x884c;&#x5207;&#x5165;&#x5f0f;&#x7f16;&#x7a0b;&#x3002;"/>
+</node>
+</node>
+<node CREATED="1539519061058" ID="ID_1425314646" MODIFIED="1539519066079" POSITION="right" TEXT="springmvc">
+<node CREATED="1539519183608" ID="ID_143778987" MODIFIED="1539519208494" TEXT="&#x4e3b;&#x8981;&#x662f;&#x8fdb;&#x884c;&#x524d;&#x540e;&#x53f0;&#x4ea4;&#x4e92;"/>
+</node>
+<node CREATED="1539519068386" ID="ID_1940693645" MODIFIED="1539519070796" POSITION="right" TEXT="mybatis">
+<node CREATED="1539519210552" ID="ID_1481365226" MODIFIED="1539519227799" TEXT="&#x7528;&#x4e8e;&#x5bf9;&#x6570;&#x636e;&#x5e93;&#x7684;&#x64cd;&#x4f5c;"/>
+</node>
+<node CREATED="1539519231497" ID="ID_1720617940" MODIFIED="1539519252656" POSITION="left" TEXT="&#x9700;&#x8981;application.yml&#x914d;&#x7f6e;&#x6587;&#x4ef6;"/>
+<node CREATED="1539519259409" ID="ID_1561110112" MODIFIED="1539519288287" POSITION="left" TEXT="&#x4f18;&#x70b9;&#xff1a;&#x914d;&#x7f6e;&#x7b80;&#x5355;&#xff0c;&#x5f00;&#x53d1;&#x8fc5;&#x901f;"/>
+</node>
+</map>
